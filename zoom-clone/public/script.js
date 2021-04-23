e6a99033cb08c154be91bcb39de26513b1dccc1c
Quan Dang <18520339@gm.uit.edu.vn>
2021-04-24 04:09:11 +0700

    zoom-clone: new user join room

--- zoom-clone/public/script.js
+++ zoom-clone/public/script.js
@@ -1,14 +1,36 @@
+const socket = io('/');
+const peer = new Peer(undefined, { path: '/peerjs', host: '/', port: 7000 });
+
 const videoGrid = document.getElementById('video-grid');
 const videoElement = document.querySelector('video');
+let videoStream = null;
 
-const addVideoStream = (video, stream) => {};
+const addVideoStream = (video, stream) => {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => video.play());
+    videoGrid.append(video);
+};
 
-const videoStream = null;
+const newVideoCall = call => {
+    const video = document.createElement('video');
+    call.on('stream', stream => addVideoStream(video, stream));
+};
+
 navigator.mediaDevices
     .getUserMedia({ video: true, audio: true })
-    .then(stream => {
+    .finally(stream => {
         videoStream = stream;
-        videoElement.srcObject = stream;
-        videoElement.onloadedmetadata(() => videoElement.play());
-        videoGrid.append(videoElement);
+        addVideoStream(videoElement, stream);
+
+        socket.on('NEW_USER', userId => {
+            const call = peer.call(userId, stream);
+            newVideoCall(call);
+        });
+
+        peer.on('call', call => {
+            call.answer(stream);
+            newVideoCall(call);
+        });
     });
+
+peer.on('open', id => socket.emit('JOIN_ROOM', ROOM_ID, id));
--- zoom-clone/server.js
+++ zoom-clone/server.js
@@ -1,16 +1,27 @@
-const express = require('express');
-const http = require('http');
 const { v4: uuidv4 } = require('uuid');
-
+const express = require('express');
 const app = express();
-const server = http.createServer(app);
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, { debug: true });
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use('/peerjs', peerServer);
 
 app.get('/', (req, res) => res.redirect(`/${uuidv4()}`));
 app.get('/:room', (req, res) => {
     res.render('room', { roomId: req.params.room });
+});
+
+io.on('connection', socket => {
+    socket.on('JOIN_ROOM', (roomId, userId) => {
+        socket.join(roomId);
+        socket.broadcast.to(roomId).emit('NEW_USER', userId);
+    });
 });
 
 const port = process.env.PORT || 7000;
